7d7de72a23354464ca908d6df790eb47f6234b51
bhkstore0-source <bhk.store0@gmail.com>
2026-04-29 10:22:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 const PRODUCT_PRICE      = 12000;
 const PRODUCT_NAME       = 'جهاز IPL لإزالة الشعر — 1,000,000 ومضة';
-const SCRIPT_URL         = 'https://script.google.com/macros/s/AKfycbwloeb03CMOOJfJ_w0dJ7nCWQrWvkJJohN5geu2m7RUp5hzynGTXYtGiRbkLQNQ-y50GA/exec';
+const SCRIPT_URL         = 'https://script.google.com/macros/s/AKfycbyoQBV3aEVZraNp8WgvMhHEdmVXGKIvH8Gor5i9bDoFUladSqrKNWZIudkdY54wGUq6bA/exec';
 const WHATSAPP_NUM       = '213553096569';
 const RESTRICTED_WILAYAS = ['52', '56', '57'];
 
